1263866a95686a56cbaa2c220c15dfa12d9c24c3
imagine10255 <imagine10255@gmail.com>
2018-05-09 11:42:45 +0800

    新增 版本1的資料格式

--- src/common/js/page/index.js
+++ src/common/js/page/index.js
@@ -1,54 +1,66 @@
 //Write something
 
 Vue.component('scheduleMatch', {
+    props: ['date', 'time', 'name1', 'name2'],
     template: `<div class="schedule-match">
                         <div class="row">
                             <div class="col-12 text-center">
-                                <span class="date">30 Jun</span>
+                                <span class="date">{{date}}</span>
                             </div>
                             <div class="col text-center order-md-0">
                                 <div class="team">
                                     <img src="./common/img/50x50.png" alt="" class="img-fluid">
-                                    <div class="name">Saudi Arabia</div>
+                                    <div class="name">{{name1}}</div>
                                 </div>
                             </div>
                             <div class="col text-center order-md-2">
                                 <div class="team">
                                     <img src="./common/img/50x50.png" alt="" class="img-fluid">
-                                    <div class="name">Saudi Arabia</div>
+                                    <div class="name">{{name2}}</div>
                                 </div>
                             </div>
                             <div class="col text-center order-md-1">
-                                <span class="time">21:00</span>
+                                <span class="time">{{time}}</span>
                             </div>
                         </div>
                     </div>`
-})
+});
 
 
-Vue.component('lineChildren', {
-    template: `<div class="link-line">
+Vue.component('lineTopChildren', {
+    template: `<div class="link-line link-line-top">
                     <div class="line-horizontal-box"></div>
                     <div class="line-center"></div>
                 </div>`
-})
+});
+
+Vue.component('lineBottomChildren', {
+    template: `<div class="link-line link-line-bottom">
+                    <div class="line-center"></div>
+                    <div class="line-horizontal-box"></div>
+                </div>`
+});
 
 
-var worldCupSchedule = new Vue({
-    el: '#world-cup-schedule',
+var app = new Vue({
+    el: '#app',
+    computed: {
+        scheduleTopData() {
+            var data = this.scheduleTop;
+            return data;
+        },
+        scheduleBottomData() {
+            //處理最後一場比賽
+            var data = this.scheduleBottom;
+            data.sort();
+            // data.shift();
+            return data;
+        }
+    },
     data: {
-        schedule: [
-            {
-                'date':'30 Jun',
-                'time':'09:00',
-                'name':'Saudi Arabia',
-            },
-            {
-                'date':'30 Jun',
-                'time':'09:00',
-                'name':'Saudi Arabia',
-            },
-        ]
+        scheduleTop: scheduleTop,
+        scheduleBottom: scheduleBottom
+
     }
 })
 
